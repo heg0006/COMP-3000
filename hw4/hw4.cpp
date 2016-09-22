e8b0df73e8944dc4e2326f951318e1f6fa191874
Hannah <heg0006@auburn.edu>
2016-09-22 11:15:04 -0500

    commiting hw4

---
+++ hw4/hw4.cpp
@@ -0,0 +1,14 @@
+// Name: Hannah Gladish
+// filename: hw4.cpp
+// Development environment: vim, g++, Linux
+// Compile: g++ -o hw4 hw4.cpp
+// Sample usage: ./hw4
+
+
+#include <iostream> 
+#include <iomanip>
+#include <stdlib.h>
+#include <assert.h>
+#include <ctime>
+
+using namespace std;
