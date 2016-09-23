d9d247901edcb029475af569818ccf9b382bfaa7
Hannah <heg0006@auburn.edu>
2016-09-23 09:57:23 -0500

    committing homework 4

--- hw4/hw4.cpp
+++ hw4/hw4.cpp
@@ -17,11 +17,95 @@ bool Aaron_alive = 0, Bob_alive = 0, Charlie_alive = 0;
 const int A_shot = 3, B_shot = 2, runs = 10000;
 
 bool two_alive(bool Aaron_alive, bool Bob_alive, bool Charlie_alive);
+void _two_alive(void);
 void first_aaron(bool& Bob_alive, bool& Charlie_alive);
 void bob_shot(bool& Aaron_alive, bool& Charlie_alive);
 void charlie_shot(bool& Aaron_alive, bool& Bob_alive);
 void second_aaron(bool& Bob_alive, bool& Charlie_alive);
+void test2(void);
+void test3(void);
+void test4(void);
+void test5(void);
+void press_key(void);
 
+int main(){
+	srand(time(0));
+	cout << "*** Welcome to Hannah Gladish's Duel Simulator ***" << endl;
+	_two_alive();
+	test2();
+	test3();
+	test4();
+	test5();
+double aaron_wins = 0;
+double bob_wins = 0;
+double charlie_wins = 0;
+double strategy1;
+double strategy2;
+int a = 0;
+	cout << "Ready to test strategy 1 (run " << runs << " times): " << endl;
+	press_key();
+	cout << ". . ." << endl <<  ". . ." << endl << ". . ." << endl;
+	while (a < runs){
+		a = a + 1;
+		Aaron_alive = true;
+		Bob_alive = true;
+		Charlie_alive = true;
+	while(two_alive(Aaron_alive, Bob_alive, Charlie_alive)){
+		if(Aaron_alive == true)
+			first_aaron(Bob_alive, Charlie_alive);
+		if(Bob_alive == true)
+			bob_shot(Aaron_alive, Charlie_alive);
+		if(Charlie_alive == true)
+			charlie_shot(Aaron_alive, Bob_alive);
+	}
+		if(Aaron_alive == true)
+			aaron_wins = aaron_wins + 1;
+		if(Bob_alive == true)
+			bob_wins = bob_wins + 1;
+		if(Charlie_alive == true)
+			charlie_wins = charlie_wins + 1;
+	}
+	strategy1 = (100 * aaron_wins)/runs;
+	cout << "Aaron won " << aaron_wins << "/" << runs << " duels or " << strategy1 << "%" << endl;
+	cout << "Bob won " << bob_wins << "/" << runs << " duels or " << (100*bob_wins)/runs << "%" << endl;
+	cout << "Charlie won " << charlie_wins << "/" << runs << " duels or " << (100*charlie_wins)/runs << "%" << endl;
+	aaron_wins = 0;
+	bob_wins = 0;
+	charlie_wins = 0;
+	a = 0;
+	cout << "Ready to test strategy 2 (run " << runs << " times): " << endl;
+	press_key();
+	cout << ". . ." << endl << ". . ." << endl << ". . ." << endl;
+	while(a < runs){
+		a = a + 1;
+		Aaron_alive = true;
+		Bob_alive = true;
+		Charlie_alive = true;
+	while(two_alive(Aaron_alive, Bob_alive, Charlie_alive)){
+		if(Aaron_alive == true)
+			second_aaron(Bob_alive, Charlie_alive);
+		if(Bob_alive == true)
+			bob_shot(Aaron_alive, Charlie_alive);
+		if(Charlie_alive == true)	
+			charlie_shot(Aaron_alive, Bob_alive);
+	}
+	if(Aaron_alive == true)
+		aaron_wins = aaron_wins + 1;
+	if(Bob_alive == true)
+		bob_wins = bob_wins + 1;
+	if(Charlie_alive == true)
+		charlie_wins = charlie_wins + 1;
+	}
+	strategy2 = (100*aaron_wins)/runs;
+	cout << "Aaron won " << aaron_wins << "/" << runs << " duels or " << strategy2 << "%" << endl;
+	cout << "Bob won " << bob_wins << "/" << runs << " duels or " << (100*bob_wins)/runs << "%" << endl;
+	cout << "Charlie won " << charlie_wins << "/" << runs << " duels or " << (100*charlie_wins)/runs << "%" << endl;
+	if (strategy1 > strategy2)
+		cout << "Strategy 1 is better than strategy 2." << endl;
+	else
+		cout << "Strategy 2 is better than strategy 1." << endl;
+	return 0;
+}
 void press_key(void){
 
 cout << "Press any key to continue...";
@@ -30,7 +114,7 @@ cout << endl << endl;
 return;
 }
 
-void two_alive(void){
+void _two_alive(void){
 	cout << "Unit Testing 1: Function - two_alive()" << endl;
 	cout << "Case 1: Aaron alive, Bob alive, Charlie alive" << endl;
 	assert(true == two_alive(true, true, true));
@@ -45,16 +129,16 @@ void two_alive(void){
 	assert(true == two_alive(true, true, false));
 	cout << "Case passed..." << endl;
 	cout << "Case 5: Aaron dead, Bob dead, Charlie alive" << endl;
-	assert(true == two_alive(false, false, true));
+	assert(false == two_alive(false, false, true));
 	cout << "Case passed ..." << endl;
 	cout << "Case 6: Aaron dead, Bob alive, Charlie dead" << endl;
-	assert(true == two_alive(false, true, false));
+	assert(false == two_alive(false, true, false));
 	cout << "Case passed ..." << endl;
 	cout << "Case 7: Aaron alive, Bob dead, Charlie dead" << endl;
-	assert(true == two_alive(true, false, false));
+	assert(false == two_alive(true, false, false));
 	cout << "Case passed ..." << endl;
 	cout << "Case 8: Aaron dead, Bob dead, Charlie dead" << endl;
-	assert(true == two_alive(false, false, false));
+	assert(false == two_alive(false, false, false));
 	cout << "Case passed ..." << endl;
 press_key();
 return;
@@ -76,7 +160,7 @@ void test2(void){
 	Charlie_alive = true;
 	first_aaron(Bob_alive, Charlie_alive);
 	assert(Bob_alive == false);
-	if(Charlie_alive == true);
+	if(Charlie_alive == true)
 		cout << "Aaron missed." << endl;
 	else	
 		cout << "Charlie is dead" << endl;
@@ -171,14 +255,15 @@ void test5(void){
 	Bob_alive = false;
 	Charlie_alive = true;
 	second_aaron(Bob_alive, Charlie_alive);
-	assert(Charlie_alive == false);
-	if(Bob_alive == true)
+	assert(Bob_alive == false);
+	if(Charlie_alive == true)
 		cout << "Aaron missed." << endl;
 	else 
-		cout << "Bob is dead." << endl;
+		cout << "Charlie is dead." << endl;
 press_key();
 return;
 }
+//Deciding who is alive
 bool two_alive(bool Aaron_alive, bool Bob_alive, bool Charlie_alive){
 
 	if((Aaron_alive && Bob_alive) || (Aaron_alive && Charlie_alive) || (Bob_alive && Charlie_alive))
@@ -186,13 +271,15 @@ bool two_alive(bool Aaron_alive, bool Bob_alive, bool Charlie_alive){
 	else
  		return false;
 }
+//Determining if Aaron's first shot is a hit 
 void first_aaron(bool& Bob_alive, bool& Charlie_alive){
 	if(Charlie_alive)
 		Charlie_alive = rand()%A_shot != 1;
 	else
-		B_alive = rand()%A_shot != 1;
+		Bob_alive = rand()%A_shot != 1;
 return;
 }
+//Determining if Bob's shot is a hit
 void bob_shot(bool& Aaron_alive, bool& Charlie_alive){
 	if(Charlie_alive)
 		Charlie_alive = rand()%B_shot != 1;
@@ -200,6 +287,7 @@ void bob_shot(bool& Aaron_alive, bool& Charlie_alive){
 		Aaron_alive = rand()%B_shot != 1;
 return;
 }
+//Determining if Charlie's shot is a hit
 void charlie_shot(bool& Aaron_alive, bool& Bob_alive){
 	if(Bob_alive)
 		Bob_alive = false;
@@ -207,6 +295,7 @@ void charlie_shot(bool& Aaron_alive, bool& Bob_alive){
 		Aaron_alive = false;
 return;
 }
+//Determining if Aaron's second shot is a hit
 void second_aaron(bool& Bob_alive, bool& Charlie_alive){
 	if((Bob_alive == true) && (Charlie_alive))
 return;
@@ -217,88 +306,3 @@ return;
 return;
 }	
 
-int main(){
-srand(time(0));
-cout << "*** Welcome to Hannah Gladish's Duel Simulator ***" << endl;
-two_alive();
-test2();
-test3();
-test4();
-test5();
-
-double aaron_wins = 0;
-double bob_wins = 0;
-double charlie_wins = 0;
-double strategy1;
-double strategy2;
-
-int i = 0;
-
-cout << "Ready to test strategy 1(run " << runs << " times):" << endl;
-press_key();
-cout << ". . ." << endl << ". . ." << endl << ". . ." << endl;
-while(i < runs){
-	i = i + 1;
-	Aaron_alive = true;
-	Bob_alive = true;
-	Charlie_alive = true;
-while(two_alive(Aaron_alive, Bob_alive, Charlie_alive)){
-	if(Aaron_alive == true)
-		first_aaron(Bob_alive, Charlie_alive);
-	if(Bob_alive == true)
-		bob_shot(Aaron_alive, Charlie_alive);
-	if(Charlie_alive == true)
-		charlie_shot(Aaron_alive, Bob_alive);
-}
-	if(Aaron_alive == true)
-		aaron_wins = aaron_wins + 1;
-	if(Bob_alive == true)
-		bob_wins = bob_wins +1;
-	if(Charlie_alive == true)
-		charlie_wins = charlie_wins + 1;
-}
-
-strategy1 = 100*aaron_wins/runs;
-cout << "Aaron won " << aaron_wins << "/" << runs << " duels or " << strategy 1 << "%" << endl;
-cout << "Bob won " << bob_wins << "/" << runs << " duels or " << 100*bob_wins/runs << "%" << endl;
-cout << "Charlie won " << charlie_wins << "/" << runs << " duels or " << 100*charlie_wins/runs << "%" << endl;
-
-aaron_wins = 0;
-bob_wins = 0;
-charlie_wins = 0;
-i = 0;
-cout << "Ready to test strategy 2 (run " << runs << " times):" << endl;
-press_key();
-
-cout << ". . ." << endl << ". . ." << endl << ". . ." << endl; 
-while(i < runs){
-i = i + 1;
-Aaron_alive = true;
-Bob_alive = true;
-Charlie_alive = true;
-
-while(two_alive(aaron_alive, bob_alive, charlie_alive)){
-	if(Aaron_alive == true)
-		second_aaron(Bob_alive, Charlie_alive);
-	if(Bob_alive == true)
-		bob_shot(Aaron_alive, Charlie_alive);
-	if(Charlie_alive == true)
-		charlie_shot(Aaron_alive, Bob_alive);
-}
-	if(Aaron_alive == true)
-		aaron_wins = aaron_wins + 1;
-	if(Bob_alive == true)
-		bob_wins = bob_wins + 1;
-	if(Charlie_alive == true)
-		charlie_wins = charlie_wins + 1;
-}
-strategy2 = 100*aaron_wins/runs;
-cout << "Aaron won " << aaron_wins << "/" << runs << " duels or " << strategy2 << "%" << endl;
-cout << "Bob won " << bob_wins << "/" << runs << " duels or " << 100*bob_wins/runs << "%" << endl;
-cout << "Charlie won " << charlie_wins << "/" << runs << " duels or " << 100*charlie_wins/runs << "%" << endl;
-if (strategy1 > strategy2)
-	cout << "Strategy 1 is better that strategy 2." << endl;
-else
-	cout << "Strategy 2 is better than strategy 1." << endl;
-return 0;
-}
